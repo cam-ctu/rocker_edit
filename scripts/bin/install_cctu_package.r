d5f5126f339062046c286a0f928236a76536782c
shug0131 <sjb277@medschl.cam.ac.uk>
2024-12-12 12:43:00 +0000

    Update install_cctu_package.r

--- scripts/bin/install_cctu_package.r
+++ scripts/bin/install_cctu_package.r
@@ -28,23 +28,23 @@ message( "R_VERSION parameter = ", args)
 
 message("pull cctu version history")
 
-df_cctu <- gert::git_remote_ls(remote = "https://github.com/cam-ctu/cctu.git")|>
-  dplyr::filter(stringr::str_detect(ref, "^refs/tags/[vV]?")) |>
-  dplyr::mutate(
-    cctu_version = stringr::str_extract(ref, r"([vV]?\d+\.\d+\.\d+.{0,1}\d*.*)"),
-    commit_url = glue::glue("https://api.github.com/repos/cam-ctu/cctu/commits/{oid}"),
-    .keep = "none"
-  ) |>
-  dplyr::slice_tail(n = 10) |>
-  dplyr::rowwise() |>
-  dplyr::mutate(cctu_commit_date = .get_github_commit_date(commit_url)) |>
-  dplyr::ungroup() |>
-  tidyr::drop_na() |>
-  dplyr::select(
-    cctu_version,
-    cctu_commit_date
-  ) |>
-  dplyr::arrange(cctu_commit_date)
+# df_cctu <- gert::git_remote_ls(remote = "https://github.com/cam-ctu/cctu.git")|>
+#   dplyr::filter(stringr::str_detect(ref, "^refs/tags/[vV]?")) |>
+#   dplyr::mutate(
+#     cctu_version = stringr::str_extract(ref, r"([vV]?\d+\.\d+\.\d+.{0,1}\d*.*)"),
+#     commit_url = glue::glue("https://api.github.com/repos/cam-ctu/cctu/commits/{oid}"),
+#     .keep = "none"
+#   ) |>
+#   dplyr::slice_tail(n = 10) |>
+#   dplyr::rowwise() |>
+#   dplyr::mutate(cctu_commit_date = .get_github_commit_date(commit_url)) |>
+#   dplyr::ungroup() |>
+#   tidyr::drop_na() |>
+#   dplyr::select(
+#     cctu_version,
+#     cctu_commit_date
+#   ) |>
+#   dplyr::arrange(cctu_commit_date)
 
 message("compare to rversion history")
 
@@ -54,18 +54,19 @@ df_args <- read.csv("build/args/history.tsv",sep="\t",
 
 message("do the matching")
 
-df_args <- df_args|>
-  tidyr::expand_grid(df_cctu)|>
-  dplyr::filter( cctu_commit_date< r_freeze_date | is.na(r_freeze_date))|>
-  dplyr::group_by(r_version) |>
-  dplyr::slice_max(cctu_commit_date, with_ties = FALSE) |>
-  dplyr::ungroup()
+# df_args <- df_args|>
+#   tidyr::expand_grid(df_cctu)|>
+#   dplyr::filter( cctu_commit_date< r_freeze_date | is.na(r_freeze_date))|>
+#   dplyr::group_by(r_version) |>
+#   dplyr::slice_max(cctu_commit_date, with_ties = FALSE) |>
+#   dplyr::ungroup()
+# 
+# cctu_version <- df_args |>
+#   dplyr::filter(r_version==args[1]) |>
+#   dplyr::pull(cctu_version)
 
-cctu_version <- df_args |>
-  dplyr::filter(r_version==args[1]) |>
-  dplyr::pull(cctu_version)
-
+cctu_version <- "0.8.1"
 message("Installing cctu version ", cctu_version)
 
 
-#remotes::install_github(repo="cam-ctu/cctu", ref=cctu_version, INSTALL_opts="--install-tests" , build_vignettes=TRUE)
+remotes::install_github(repo="cam-ctu/cctu", ref=cctu_version, INSTALL_opts="--install-tests" , build_vignettes=TRUE)
